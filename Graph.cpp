737dcc8083ade0c08e8ddea021d91d798f82b922
Artem <ashumanov@gmail.com>
2016-11-04 00:17:04 +0300

    The find clicks in the graphAdjacency was added.

--- Graph.cpp
+++ Graph.cpp
@@ -1,4 +1,55 @@
-//
-// Created by asot on 03.11.16.
-//
+#include <vector>
+#include <iostream>
 
+struct Triangle {
+    int onePoint;
+    int twoPoint;
+    int threePoint;
+};
+
+class Graph {
+private:
+    int n;
+    int *nodes;
+public:
+    virtual bool isLink(int n1, int n2) = 0;
+
+    Graph(int n) : n(n) {
+        nodes = new int[n];
+        for (auto i = 0; i < n; ++i) {
+            nodes[i] = i + 1;
+        }
+    }
+
+    std::vector<Triangle> getTriangles() {
+        std::vector<Triangle> triangles;
+        for (int i = 0; i < n - 2; ++i) {
+            for (int j = i + 1; j < n - 1; ++j) {
+                for (int k = j + 1; k < n; ++k) {
+                    if (isLink(i, j) && isLink(i, k) && isLink(j, k)) {
+                        Triangle triangle;
+                        triangle.onePoint = i + 1;
+                        triangle.twoPoint = j + 1;
+                        triangle.threePoint = k + 1;
+                        triangles.push_back(triangle);
+                    }
+                }
+            }
+        }
+        return triangles;
+    }
+};
+
+class GraphAdjacency : public Graph {
+public:
+    GraphAdjacency(int n, int** graph) : Graph(n) {
+        matrix = graph;
+    }
+
+private:
+    int** matrix;
+
+    virtual bool isLink(int n1, int n2) override {
+        return matrix[n1][n2] == 1;
+    }
+};
--- main.cpp
+++ main.cpp
@@ -1,6 +1,42 @@
 #include <iostream>
+#include "Graph.cpp"
+
+void printTriangles(std::vector<Triangle> vector) {
+    for (int i = 0; i < vector.size(); ++i) {
+        Triangle triangle = vector[i];
+        std::cout << "Triangle: " << triangle.onePoint << " " << triangle.twoPoint << " "
+                  << triangle.threePoint << "\n";
+    }
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int size = 5;
+    int graph[][size] = {{0, 1, 0, 1, 1},
+                         {1, 0, 1, 0, 0},
+                         {0, 1, 0, 0, 0},
+                         {1, 0, 0, 0, 1},
+                         {1, 0, 0, 1, 0}};
+
+    int **a = new int *[size];
+    for (int i = 0; i < size; i++)
+        a[i] = new int[size];
+
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < size; ++j) {
+            a[i][j] = graph[i][j];
+        }
+    }
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < size; ++j) {
+            std::cout << a[i][j] << " ";
+        }
+        std::cout << "\n";
+    }
+    std::cout << "\n";
+
+    GraphAdjacency graphAdjacency(size, a);
+    const std::vector<Triangle> vector = graphAdjacency.getTriangles();
+    printTriangles(vector);
     return 0;
 }
+
